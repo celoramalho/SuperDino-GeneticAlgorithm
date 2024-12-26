2df674a7fc3f27cac4723f23cdaf37de00e156d7
anubis_greyignis <marceloramalho.info@gmail.com>
2024-12-26 20:25:46 -0300

    Convolution Kernel types, filter matrix like Laplacian, Kirsch, Sobel, Emboss etc

--- screen_capture/convolution_kernel.py
+++ screen_capture/convolution_kernel.py
@@ -3,5 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class ConvolutionKernel:
-    def __init__(self):
-        pass
+    Laplacian = np.array([[0, 1, 0], 
+                      [1, -4, 1], 
+                      [0, 1, 0]])
+    Sobel_X = np.array([[-1, 0, 1], 
+                    [-2, 0, 2], 
+                    [-1, 0, 1]])
+
+    Sobel_Y = np.array([[-1, -2, -1], 
+                        [ 0,  0,  0], 
+                        [ 1,  2,  1]])
+    Emboss = np.array([[-2, -1,  0],
+                    [-1,  1,  1],
+                    [ 0,  1,  2]])
+    Kirsch = np.array([[ 5,  5,  5], 
+                    [-3,  0, -3], 
+                    [-3, -3, -3]])
+    Prewitt_X = np.array([[-1, 0, 1],
+                        [-1, 0, 1],
+                        [-1, 0, 1]])
+    Prewitt_Y = np.array([[-1, -1, -1], 
+                        [ 0,  0,  0], 
+                        [ 1,  1,  1]])
+    kernels = {
+        "Laplacian": Laplacian,
+        "Sobel X": Sobel_X,
+        "Sobel Y": Sobel_Y,
+        "Emboss": Emboss,
+        "Kirsch Compass": Kirsch,
+        "Prewitt X": Prewitt_X,
+        "Prewitt Y": Prewitt_Y
+    }
+    
